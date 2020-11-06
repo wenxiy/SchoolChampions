28590feb3d25061b8cd6f70ba2599892c8540338
wenxin <193367189@qq.com>
2020-11-06 20:10:57 +0800

    join the bottomnavigition

--- app/src/main/java/com/example/schoolchampionship/MainActivity.java
+++ app/src/main/java/com/example/schoolchampionship/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.schoolchampionship;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
+    private ViewPager select_viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initview();
+    }
+
+    private void initview() {
+        bottomNavigationView = findViewById(R.id.navigation_container);
     }
 }
