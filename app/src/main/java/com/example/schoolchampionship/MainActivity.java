57434e0d7688d836122cf47280c12c069ec8d929
wenxin <193367189@qq.com>
2020-11-06 20:48:01 +0800

    “搭架子，将fragment放进去”

--- app/src/main/java/com/example/schoolchampionship/MainActivity.java
+++ app/src/main/java/com/example/schoolchampionship/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.schoolchampionship;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
@@ -10,14 +11,23 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class MainActivity extends AppCompatActivity {
     private BottomNavigationView bottomNavigationView;
     private ViewPager select_viewPager;
+    private StudyFragment studyFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initview();
+        initclick();
+    }
+
+    private void initclick() {
+
     }
 
     private void initview() {
         bottomNavigationView = findViewById(R.id.navigation_container);
+        studyFragment = new StudyFragment();
+        FragmentManager fm = getSupportFragmentManager();
+        fm.beginTransaction().replace(R.id.fragment_container,studyFragment).commit();
     }
 }
